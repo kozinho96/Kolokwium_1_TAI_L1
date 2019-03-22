f81daafc575326dee0e892ebc6250bfbb5c50ef1
kozinho <kozinho96@Interia.pl>
2019-03-22 15:54:25 +0100

    kolokwium

---
+++ navbar.js
@@ -0,0 +1,6 @@
+let nav = '<h1>Nawigacja</h1>' +
+          '<a href="index.html">Index </a>' +
+          '<a href="kontakt.html"> Kontakt</a>';
+
+let navVariable = document.querySelector(".nav-bar");
+navVariable.innerHTML = nav;
---
+++ script.js
@@ -0,0 +1,6 @@
+let box = document.querySelector('.box');
+let button = document.querySelector('.button')
+
+button.addEventListener('click',function () {
+    box.className = 'boxRed';
+});
